d01f1b3dc938c2a9246c342eb6a84827cb09d7bf
João <93404889+JoaoRotger@users.noreply.github.com>
2022-08-15 16:07:36 -0300

    Add files via upload

---
+++ print('Hello World').py
@@ -0,0 +1,24 @@
+print('Hello World')
+
+print("Brasil", "ganhou", 5, "titulos mundiais", sep="-")
+
+print("Brasil", "ganhou", 5, "titulos mundiais", end=". ")
+
+pais = 'Italia'
+quantidade = 4
+print(pais, 'ganhou', quantidade, 'titulos mundiais')
+
+#pais = input('')
+#if pais == 'Italia'(
+#    print('Italia ganhou 4 copas')
+#    else if pais == 'Brasil'(print('Brasil ganhou 5 copas'))
+#    else = print('Não sei quantas copas esse pais ganhou')
+#)
+
+#print data
+
+dia = 15
+mes = 8
+ano = 2022
+
+print(dia, mes, ano, sep = '/')
